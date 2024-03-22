9b39cba7b461228e948123a157d6e137a0e1c40d
jbcollins254@gmail.com <jbcollins254@gmail.com>
2024-03-22 11:24:24 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const myArray = []
+function writeCards(newArray, eventName){
+    for(let i = 0;i < newArray.length; i++){
+      let message =`Thank you, ${newArray[i]}, for the wonderful ${eventName} gift!`
+    
+      myArray.push(message);
+    } 
+   return myArray 
+}
+function countDown(number){
+    while(number >= 0 && number <= 10){
+        console.log(number--)
+    }
+}
+
+
+
+
+
+
